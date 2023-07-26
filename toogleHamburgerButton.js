cd175f0e4750d95fe294aabaa007e979a105fbf9
chrillah <ch.ronnberg@gmail.com>
2023-07-26 20:57:41 +0200

    drum

--- toogleHamburgerButton.js
+++ toogleHamburgerButton.js
@@ -7,10 +7,32 @@ function closeMenu() {
     menu.style.display = 'none'
 }
 
+// ETT PROBLEM tog upp all min fokus då jag upptäckte en bugg
+// Buggen var att jag hade satt en media-querrie på själva länk-menyn, som ändrade synligheten från synlig(grid)
+// till none. Men när jag förstorade skärmen, så försvann länk-menyn
+// det är bara för att jag styr länk-menyn om den ska synas eller inte med javascript. och eftersom javascript
+// Skriver över csss så skrves även min media querrie.
+// så min lösning är att jag fick helt enkelt skapa en if-statement som kollar ifall hamburgarknappen är synlig eller inte, det funkade inte
+
+
 // den här funktionen gör så att den selekterande menyn syns
 function openMenu() {
-    menu.style.display = 'grid'
+    menu.style.display = 'block'
 }
 
 // den selekterande menyn
 const menu = document.getElementById('menu')
+
+// vi selekterar hamburgarknappen
+const hamburgerButton = document.getElementById("hamburger")
+
+hamburgerButton.addEventListener('click', ()=>{
+    const isOpened = hamburgerButton.getAttribute('aria-expanded')
+    if(isOpened === 'false'){
+        hamburgerButton.setAttribute('aria-expanded', 'true')
+        openMenu();
+    } else {
+        hamburgerButton.setAttribute('aria-expanded', 'false')
+        closeMenu();
+    }
+})
