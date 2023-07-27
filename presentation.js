8cb6c536569321f853d50a6036e63fa1eb06b89a
chrillah <ch.ronnberg@gmail.com>
2023-07-28 00:25:37 +0200

    fix

--- presentation.js
+++
@@ -1,4 +0,0 @@
-// den här scripten är endast programmerad för att visa upp mina kunskaper, jag har samlat på mig
-// men också för att inte ha så mycket text i html-koden, för att enkelt kunna ha isär och underhålla
-
-console.log('PRESENTATION OCH REDOVISNING')
