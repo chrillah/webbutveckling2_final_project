ff35435a90a63bcf384ef5737e652ebc5c570b12
chrillah <ch.ronnberg@gmail.com>
2023-07-27 18:29:21 +0200

    index klar

--- drummachine.js
+++ drummachine.js
@@ -3,47 +3,49 @@ function playSound(e) {
     // vi selecterar audio-elementet med dataattributets tangentkod
     // vi låter e, vilket triggas av en tangentnedtryckning, koppla key code med rätt tanget så att ljudet som är kopplat till key code kan spelas upp
     // vi använder backticks så att vi kan sammanfoga tangentkoden
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
     // här selekterar vi själva tangenten för att kunna applicera en klass för att animera knappnedtryckningen
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
     //  vi använder ett if-statement som kollar så att den tangent vi rycker ner är kopplat till en aduio-element.
     // om det inte finns en audio-element, stoppar if-statement funktionen
-    if (!audio) return
+    if (!audio) return;
 
     // Varje ljud tillåts att spela en gång, därefter slutar ljudet att låta
     // för att tillåta ljudet spela flera än en gång, måste man starta om
-    audio.currentTime = 0
+    audio.currentTime = 0;
 
     // denna funktion finns inbyggd i javascript och tillåter att ljud hörs
-    audio.play()
+    audio.play();
 
     // vi lägger till klassen "playing" som gör att knappen animeras
     // denna klass har en transition, en övergång. En egenskap som möjliggör animation
-    key.classList.add('playing')
+    key.classList.add('playing');
 }
 // funktion som körs när en övergång är på väg att ta slut
 function removeTransition(e) {
     // om en tangentknapp inte innehåller en övergång med egenskapen transform,
     // så skippar if-statement detta
-    if (e.propertyName !== 'transform') return
+    if (e.propertyName !== 'transform') return;
 
     // this är en nyckelord i Javscript som refererar till en specifik variabel.
     // i detta fall, så pekar Javascript på just den tangent som är aktiv
     // på så sätt kan vi då kunna ta bort klassen playing från just den tangenten
-    this.classList.remove('playing')
+    this.classList.remove('playing');
 }
 
 // vi selekterar alla tangenter med klassen "key"
-const keys = document.querySelectorAll('.key')
+var keys = document.querySelectorAll('.key');
 // här använder vi en foreach-loop, en loop som främst används för att loopa igenom arrayer, listor och object
 // Vi ska lägga till en event som heter transitionend, ett event som triggas när en övergång är slut
 // vi loopar och lägger till en addEventListener, som triggar igång ett event när en transition är över, och då körs funktionen removeTransition
-keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 
 // här så lyssnar javascript efter events tangentnedtryckningar.
 // denna event triggas nu av tangentnedtryckningar.
 // vi skickar vad som ska triggas med en funktion som ger oss ett event
-window.addEventListener('keydown', playSound)
+window.addEventListener('keydown', playSound);
 
+
+// Validerad 27/7 2023 med JSHint - "klagade" på att jag använde template literals och arrow-funktion
--- toogleHamburgerButton.js
+++ toogleHamburgerButton.js
@@ -4,35 +4,57 @@
 
 // den här funktionen gör så att den selekterande menyn inte syns
 function closeMenu() {
-    menu.style.display = 'none'
+    menu.style.display = 'none';
 }
-
-// ETT PROBLEM tog upp all min fokus då jag upptäckte en bugg
-// Buggen var att jag hade satt en media-querrie på själva länk-menyn, som ändrade synligheten från synlig(grid)
-// till none. Men när jag förstorade skärmen, så försvann länk-menyn
-// det är bara för att jag styr länk-menyn om den ska synas eller inte med javascript. och eftersom javascript
-// Skriver över csss så skrves även min media querrie.
-// så min lösning är att jag fick helt enkelt skapa en if-statement som kollar ifall hamburgarknappen är synlig eller inte, det funkade inte
-
 
 // den här funktionen gör så att den selekterande menyn syns
 function openMenu() {
-    menu.style.display = 'block'
+    menu.style.display = 'block';
+}
+
+// eventet triggar igång denna funktion
+function expandButton() {
+
+    // hämtar hamburgarens attribut och sparar det i en variabel
+    var isOpened = hamburgerButton.getAttribute('aria-expanded');
+
+    // kollar värdet på atttributet
+    if (isOpened === 'false') {
+        hamburgerButton.setAttribute('aria-expanded', 'true');
+        openMenu();
+    } else {
+        hamburgerButton.setAttribute('aria-expanded', 'false');
+        closeMenu();
+    }
+}
+
+// buggfixet som kollar om hamburgaknappen är synlig eller inte och ändrar då menyns exponering
+function isItOpen(){
+
+    // hämtar hamburgarens displayvärde och sparar det i en variabel
+    var display = window.getComputedStyle(hamburgerButton).display;
+
+    // kollar om displayvärdet är "none" och utför beroende på läget
+    if(display === "none"){
+        openMenu();
+    } else{
+        // Syns knappen, så ändras knappen till hamburgaren och menyn syns inte
+        hamburgerButton.setAttribute('aria-expanded', 'false');
+        closeMenu();
+    }
 }
 
 // den selekterande menyn
-const menu = document.getElementById('menu')
+var menu = document.getElementById('menu');
 
-// vi selekterar hamburgarknappen
-const hamburgerButton = document.getElementById("hamburger")
+// selekterar hamburgarknappen
+var hamburgerButton = document.getElementById('hamburger');
 
-hamburgerButton.addEventListener('click', ()=>{
-    const isOpened = hamburgerButton.getAttribute('aria-expanded')
-    if(isOpened === 'false'){
-        hamburgerButton.setAttribute('aria-expanded', 'true')
-        openMenu();
-    } else {
-        hamburgerButton.setAttribute('aria-expanded', 'false')
-        closeMenu();
-    }
-})
+// det som triggar igång eventet
+window.addEventListener("resize", isItOpen);
+
+// tilldelar ett addEventListiner
+hamburgerButton.addEventListener('click', expandButton);
+
+
+// Validerad 27/7 2023 med JSHint
